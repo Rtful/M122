8b47e3df8a1e76fa6aab314df88106afea5f7c46
Aaron Holenstein <4aron.dodger@gmail.com>
2023-06-29 16:07:32 +0200

    2023-06-29 16:07:32

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# System Variables are used: 1.7
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then # Windows
+    echo "Windows hat keine cronjobs. Leider funktioniert dieses Script nicht auf dieser Plattform";
+    exit 1;
+fi
+
 # Make the second script executable
 chmod +x gitUp.sh
 
@@ -7,10 +13,11 @@ chmod +x gitUp.sh
 CRON_FILE=$(mktemp)
 
 # Add the cron job to execute the second script every hour
-echo "0 * * * * \$PWD/gitUp.sh" > "$CRON_FILE"
+echo "* 0-23 * * * \$PWD/gitUp.sh" > "$CRON_FILE"
 
 # Install the cron job
 crontab "$CRON_FILE"
 
 # Remove the temporary cron file
 rm "$CRON_FILE"
+exit 0;
