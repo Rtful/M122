426fe867de7f937e9b19c2c845fec2ceecd263af
Pandouby <57749377+Pandouby@users.noreply.github.com>
2023-06-29 16:01:41 +0200

    2023-06-29 16:01:41

--- gitUp.sh
+++ gitUp.sh
@@ -5,15 +5,19 @@ source commit.sh
 config_file="config.txt"
 error_file="error.txt"
 
-
 # Check if config file exists
+# Data is written to a file 1.3
 if [ ! -f "$config_file" ]; then
-    echo "Error: Config file not found." >> error.txt
+    echo "Error: Config file not found." >> $error_file
+    #Tail is used 1.5
+    tail -n 1 $error_file
     exit 1
 fi
 if [ -z "$config_file" ]; then
-    echo "Error: Config file is empty" >> error.txt
+    echo "Error: Config file is empty" >> $error_file
+    tail -n 1 $error_file
     exit 1
 fi
 
+# Data form a file is read 1.3
 commit $(cat $config_file)
